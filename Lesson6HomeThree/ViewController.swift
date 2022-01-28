4ddfd9cea5d8358108bfeb757aeabdfc31e11aee
Мусаев94 <beksultan1923@gmail.com>
2022-01-28 14:53:16 +0600

    Initial Commit

---
+++ Lesson6HomeThree/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson6HomeThree
+//
+//  Created by user on 28/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
